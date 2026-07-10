ab54e514a6a27ac35fbf2248085491a55085b499
Bus693 <noemail>
2026-07-10 14:41:18 -0700

    Allow mod grouping

--- run_balatro.py
+++ run_balatro.py
@@ -1,6 +1,7 @@
 from argparse import ArgumentParser
 from pathlib import Path
 from subprocess import run
+from sys import exit, stderr
 
 MODS_SRC_PATH = "~/Documents/balatro/mods/"
 MODS_INSTALL_PATH = "~/Library/Application Support/Balatro/Mods/"
@@ -14,27 +15,35 @@ RUN_TYPES = ["no_mods", "core", "all"]
 verbose = None
 
 def __clean_up_mods():
+  if verbose:
+    print("Cleaning up")
   mods_install_root_path = Path(MODS_INSTALL_PATH).expanduser()
   for item in mods_install_root_path.iterdir():
     if not item.is_symlink():
       continue
     if verbose:
-      print(f"Removing symlink for {item.name}")
+      print(f"  Removing symlink for {item.name}")
     item.unlink()
 
 def __install(dir_name):
   src_folder_path = Path(f"{MODS_SRC_PATH}{dir_name}/").expanduser()
+  # no directory traversal outside MODS_SRC_PATH
+  is_safe_location = src_folder_path.resolve().is_relative_to(Path(MODS_SRC_PATH).expanduser().resolve())
+  if not is_safe_location:
+    print(f"Location not allowed {dir_name}", file=stderr)
+    print("Aborting", file=stderr)
+    exit(1)
   for item in src_folder_path.iterdir():
     if not item.is_dir():
       continue
     posix_target = Path(f"{MODS_INSTALL_PATH}/{item.name}").expanduser()
     posix_target.symlink_to(item)
     if verbose:
-      print(f"Creating symlink for {item.name}")
+      print(f"  Creating symlink for {item.name}")
       
 def main():
   parser = ArgumentParser()
-  parser.add_argument("run_type", nargs="?", default="no_mods", choices=["no_mods", "core", "all"])
+  parser.add_argument("groups", nargs="*")
   parser.add_argument("-v", "--verbose", action="store_true")
   args = parser.parse_args()
   global verbose
@@ -42,7 +51,7 @@ def main():
 
   print("Running Balatro via bus693/balatro-runner")
 
-  if args.run_type == "no_mods":
+  if not len(args.groups):
     run(["open", "-a", "Balatro"])
     return
 
@@ -50,11 +59,11 @@ def main():
     print("Installing mods...")
   __clean_up_mods()
 
-  if args.run_type == "core":
-    __install("core")
-  elif args.run_type == "all":
-    __install("all")
-    __install("core")
+  for group in args.groups:
+    # TODO: stop .. traversal
+    if verbose:
+      print(f"Installing group {group}")
+    __install(group)
 
   if verbose:
     print("Done installing mods.")
