342161c09fcb8a759dd73181c56311efdaf0057f
Bus693 <noemail>
2026-07-12 18:47:44 -0700

    Use groups.json to configure groups instead of folders

--- run_balatro.py
+++ run_balatro.py
@@ -1,17 +1,19 @@
 from argparse import ArgumentParser
+from enum import Enum
+from json import dumps, loads
 from pathlib import Path
 from subprocess import run
 from sys import excepthook, exit, stderr
 
-MODS_SRC_PATH = "~/Documents/balatro/mods/"
-MODS_INSTALL_PATH = "~/Library/Application Support/Balatro/Mods/"
+MODS_SRC_PATH = Path("~/Documents/balatro/mods/").expanduser()
+MODS_INSTALL_PATH = Path("~/Library/Application Support/Balatro/Mods/").expanduser()
 LOVELY_BLACKLIST_PATH = "~/Library/Application Support/Balatro/Mods/lovely/blacklist.txt"
 LOVELY_RUNNER_PATH = "~/Library/Application Support/Steam/steamapps/common/Balatro/run_lovely_macos.sh"
 
-# no_mods: no mods at all, not even lovely
-# core: vanilla with a few QOL mods
-# all: all the mods, toggle individual mods using smods in-game mod selector
-RUN_TYPES = ["no_mods", "core", "all"]
+class EXIT_CODES(Enum):
+  DEPTH_EXCEEDED = 3,
+  MOD_OUTSIDE_MODS_FOLDER = 4,
+  SIGINT = 130,
 
 verbose = None
 
@@ -26,21 +28,51 @@ def __clean_up_mods():
       print(f"  Removing symlink for {item.name}")
     item.unlink()
 
-def __install(dir_name):
-  src_folder_path = Path(f"{MODS_SRC_PATH}{dir_name}/").expanduser()
-  # no directory traversal outside MODS_SRC_PATH
-  is_safe_location = src_folder_path.resolve().is_relative_to(Path(MODS_SRC_PATH).expanduser().resolve())
-  if not is_safe_location:
-    print(f"Location not allowed {dir_name}", file=stderr)
-    print("Aborting", file=stderr)
-    exit(1)
-  for subdir in src_folder_path.iterdir():
-    if not subdir.resolve().is_dir():
-      continue
-    posix_target = Path(f"{MODS_INSTALL_PATH}/{subdir.name}").expanduser()
-    posix_target.symlink_to(subdir)
+def __install(group_name, mods):
+  print(group_name, mods)
+  for mod in mods:
+    src_folder = MODS_SRC_PATH / mod
+    # no directory traversal outside MODS_SRC_PATH
+    is_safe_location = src_folder.resolve().is_relative_to(MODS_SRC_PATH)
+    if not is_safe_location:
+      print(f"Location not allowed {mod}", file=stderr)
+      print("Aborting", file=stderr)
+      exit(EXIT_CODES.MOD_OUTSIDE_MODS_FOLDER.value)
+
+    if not src_folder.resolve().is_dir():
+      # TODO: install
+      print(f"Cannot find mod {mod}, skipping", file=stderr)
+
+    target_mod_name = "@".join(mod.split("/"))
+    posix_target = MODS_INSTALL_PATH / target_mod_name
     if verbose:
-      print(f"  Creating symlink for {subdir.name}")
+      print(f"  Creating symlink for {mod}")
+    posix_target.symlink_to(src_folder)
+
+def __get_group_mods_from_config():
+  groups_config_path = Path(__file__).parent / "groups.json"
+  with open(groups_config_path) as f:
+    groups_config = loads("".join(f.readlines()))
+  group_mods = {}
+  for group_name in groups_config:
+    group_mods[group_name] = __get_group_mods(group_name, groups_config)
+  return group_mods
+
+def __get_group_mods(group_name, groups_config, depth=0):
+  max_depth = 10
+  if depth > max_depth:
+    print(f"{group_name} within the configs is nested too deep", file=stderr)
+    print(f"Aborting", file=stderr)
+    exit(EXIT_CODES.DEPTH_EXCEEDED.value)
+  mods = []
+  group = groups_config[group_name]
+  if "mods" in group:
+    mods.extend(group["mods"])
+  if "groups" in group:
+    # TODO: memoize
+    for subgroup in group["groups"]:
+      mods.extend(__get_group_mods(subgroup, groups_config, depth+1))
+  return mods
 
 def __clear_blacklist():
   blacklist_loc = Path("~/Library/Application Support/Balatro/Mods/lovely/blacklist.txt").expanduser()
@@ -70,14 +102,19 @@ def main():
     return
 
   if verbose:
+    print("Parsing config")
+  group_mods = __get_group_mods_from_config()
+  if verbose:
+    print(dumps(group_mods, indent=2))
+
+  if verbose:
     print("Installing mods...")
   __clean_up_mods()
 
-  for group in args.groups:
-    # TODO: stop .. traversal
+  for group_name in args.groups:
     if verbose:
-      print(f"Installing group {group}")
-    __install(group)
+      print(f"Installing group {group_name}")
+    __install(group_name, group_mods[group_name])
 
   if args.clear_blacklist:
     if verbose:
@@ -97,4 +134,4 @@ try:
   main()
 except KeyboardInterrupt:
   print("\nInterrupted by user, closing Balatro.", file=stderr)
-  exit(130)
+  exit(EXIT_CODES.SIGINT.value)
